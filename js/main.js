db4879a89ad3c832d009225491acc39614aa48c6
jboyd8 <jamieboyd8@yahoo.com>
2019-08-08 11:53:49 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.querySelector('#temp-form').addEventListener('submit', function(e) {
+
+    
+    
+    //Stop results disappearing as soon as they are displayed.
+    e.preventDefault();
+})
