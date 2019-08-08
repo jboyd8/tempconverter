faaed5b6cef345c23945a4b12ee0adc9aaadd022
jboyd8 <jamieboyd8@yahoo.com>
2019-08-08 12:00:07 +0100

    added loading image to index.html in a div and event listener in js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-document.querySelector('#temp-form').addEventListener('submit', function(e) {
+document.querySelector('#temp-form-c2f').addEventListener('submit', function(e) {
 
-    
+    //Make laoding img appear when submit clicked
+    document.querySelector('#loading-c2f').style.display = 'block';
+
+    //Image will only appear for 2 seconds.
+    setTimeout(calculateTempCelciusToFarenheit, 2000);
     
     //Stop results disappearing as soon as they are displayed.
     e.preventDefault();
